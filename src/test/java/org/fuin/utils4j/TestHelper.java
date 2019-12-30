d4692f244fb5f0636d33bafa0b68c09802698078
Michael Schnell <michael@fuin.org>
2019-12-30 10:44:51 +0100

    Fixed typo

--- src/test/java/org/fuin/utils4j/TestHelper.java
+++ src/test/java/org/fuin/utils4j/TestHelper.java
@@ -29,7 +29,7 @@ import java.util.Properties;
 public final class TestHelper {
 
     /**
-     * Private constructor to vaoid external instanciation.
+     * Private constructor to avoid external instantiation.
      */
     private TestHelper() {
         super();
