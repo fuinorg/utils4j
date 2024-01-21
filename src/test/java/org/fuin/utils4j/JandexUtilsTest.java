e7ab6c3d216620b3fdd004db645d4cafde663fb8
Michael Schnell <michael@fuin.org>
2024-01-21 08:57:15 +0100

    Added new methods

--- src/test/java/org/fuin/utils4j/JandexUtilsTest.java
+++ src/test/java/org/fuin/utils4j/JandexUtilsTest.java
@@ -23,6 +23,7 @@ import org.jboss.jandex.Indexer;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -36,12 +37,18 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 // CHECKSTYLE:OFF
 public class JandexUtilsTest {
 
+    private static final File TARGET_DIR = new File("target");
+
+    private static final File CLASSES_DIR = new File(TARGET_DIR, "classes");
+
+    private static final File TEST_CLASSES_DIR = new File(TARGET_DIR, "test-classes");
+
     @Test
     public final void testIndexClassFile() {
 
         // PREPARE
         final List<File> knownFiles = new ArrayList<File>();
-        final File classFile = new File("target/classes/" + JandexUtils.class.getName().replace('.', '/') + ".class");
+        final File classFile = new File(CLASSES_DIR, JandexUtils.class.getName().replace('.', '/') + ".class");
         final Indexer indexer = new Indexer();
 
         // TEST
@@ -58,11 +65,8 @@ public class JandexUtilsTest {
     @Test
     public final void testIndexDirSimple() {
 
-        // PREPARE
-        final File classesDir = new File("target/classes");
-
         // TEST
-        final Index index = JandexUtils.indexDir(classesDir);
+        final Index index = JandexUtils.indexDir(CLASSES_DIR);
 
         // VERIFY
         assertThat(index.getClassByName(DotName.createSimple(JandexUtils.class.getName()))).isNotNull();
@@ -74,8 +78,8 @@ public class JandexUtilsTest {
     public final void testIndexDir() {
 
         // PREPARE
-        final File jandexClassFile = new File("target/classes/" + JandexUtils.class.getName().replace('.', '/') + ".class");
-        final File utils4JClassFile = new File("target/classes/" + Utils4J.class.getName().replace('.', '/') + ".class");
+        final File jandexClassFile = new File(CLASSES_DIR, JandexUtils.class.getName().replace('.', '/') + ".class");
+        final File utils4JClassFile = new File(CLASSES_DIR, Utils4J.class.getName().replace('.', '/') + ".class");
         final List<File> knownFiles = new ArrayList<File>();
         final File classesDir = new File("target/classes");
         final Indexer indexer = new Indexer();
@@ -115,7 +119,7 @@ public class JandexUtilsTest {
     public final void testIndexClasspath() throws IOException {
 
         // PREPARE
-        final File jandexClassFile = new File("target/classes/" + JandexUtils.class.getName().replace('.', '/') + ".class")
+        final File jandexClassFile = new File(CLASSES_DIR, JandexUtils.class.getName().replace('.', '/') + ".class")
                 .getCanonicalFile();
         final List<File> knownFiles = new ArrayList<File>();
         final Indexer indexer = new Indexer();
@@ -140,6 +144,57 @@ public class JandexUtilsTest {
         assertThatThrownBy(() -> JandexUtils.loadClass(DotName.createSimple("a.b.c.d.DoesNotExist")))
                 .isInstanceOf(RuntimeException.class)
                 .hasMessageContaining("Failed to load class");
+    }
+
+    @Test
+    void testWriteIndexR() throws IOException {
+
+        // PREPARE
+        final Indexer indexer = new Indexer();
+        indexer.indexClass(JandexUtils.class);
+        final Index index = indexer.complete();
+        final String filename = this.getClass().getSimpleName() + ".index";
+        final File file = new File(CLASSES_DIR, filename);
+        file.delete();
+
+        // TEST
+        JandexUtils.writeIndexR(file, index);
+
+        // VERIFY
+        assertThat(file).exists();
+        final Index result = JandexUtils.loadIndexResourceR(filename);
+        assertThat(result.getClassByName(JandexUtils.class.getName())).isNotNull();
+        assertThat(result.getClassByName(JandexUtils.class.getName()).name().toString()).isEqualTo(JandexUtils.class.getName());
+
+    }
+
+    @Test
+    public final void loadIndexResource() throws IOException {
+        final Index index = JandexUtils.loadIndexResource("sample.index");
+        assertThat(index.getClassByName(JandexUtils.class.getName())).isNotNull();
+        assertThat(index.getClassByName(JandexUtils.class.getName()).name().toString()).isEqualTo(JandexUtils.class.getName());
+    }
+
+    @Test
+    public final void loadIndexResourceR() {
+        final Index index = JandexUtils.loadIndexResourceR("sample.index");
+        assertThat(index.getClassByName(JandexUtils.class.getName())).isNotNull();
+        assertThat(index.getClassByName(JandexUtils.class.getName()).name().toString()).isEqualTo(JandexUtils.class.getName());
+    }
+
+    @Test
+    public final void loadIndexResourceFailure() {
+        assertThatThrownBy(() -> JandexUtils.loadIndexResource("does-not-exist.index"))
+                .isInstanceOf(FileNotFoundException.class)
+                .hasMessage("Resource not found: does-not-exist.index");
+    }
+
+    @Test
+    public final void loadIndexResourceRFailure() {
+        assertThatThrownBy(() -> JandexUtils.loadIndexResourceR("does-not-exist.index"))
+                .isInstanceOf(RuntimeException.class)
+                .hasRootCauseInstanceOf(FileNotFoundException.class)
+                .hasRootCauseMessage("Resource not found: does-not-exist.index");
     }
 
 }
